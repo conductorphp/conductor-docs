332ef996ee7ade9070ca5bff8372d3c1e81c51b8
Robofirm.com Production <webuser@web1.production.robofirm.com>
2018-09-07 09:57:56 -0400

    Added module documentation management from docs contained within Conductor modules

---
+++ install-module-docs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# @see https://gist.github.com/pkuczynski/8665367
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+eval $(parse_yaml mkdocs.yml "config_")
+
+for dir in vendor/conductor/*/ ; do
+    module=${dir:17:-1}
+    varName="config_module_path_map_$module"
+    path=${!varName}
+    if [[ -z $path ]]; then
+      echo -e "${RED}Conductor module \"$module\" not defined in module_path_map. Add module_path_map/$module to mkdocs.yml.${NC}"
+      continue
+    fi
+    rsync -r "$dir/docs/" "docs/modules/$path"
+done
---
+++ update-module-docs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
+    branch=${branch:11}
+    if [[ ! 'release-' = ${branch:0:8} ]]; then
+      continue
+    fi
+
+    version=${branch:8}
+    echo "Updating $version docs..."
+    {
+        git checkout $branch
+        composer update
+        ./install-module-docs.sh
+        mike deploy $version -p
+        git add .
+        git commit -m "Updated module docs"
+        git push
+    }
+    echo "$version docs deployed."
+done
